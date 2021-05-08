e4f0d6de82aaee90ac68f7cd0c05dec16dac7fa2
Clauê Silveira <claue.silveira@gmail.com>
2021-05-08 12:49:02 -0300

    Feat: Tela de Signin

--- App.tsx
+++ App.tsx
@@ -1,30 +1,22 @@
-
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet } from 'react-native';
 import Constants from 'expo-constants';
-import { Signin } from './src/pages/sign-in/Signin';
+
+import { SignIn } from './src/pages';
 
 export default function App() {
   return (
     <SafeAreaView style={styles.safeContent}>
-      <View style={styles.container}>
-        <StatusBar style="auto" />
-        <Signin />
-      </View>
+      <StatusBar />
+      <SignIn />
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#55f',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
   safeContent: {
     marginTop: Constants.statusBarHeight,
     flex: 1,
-  },
+  }
 });
--- src/pages/sign-in/Signin.tsx
+++ src/pages/sign-in/Signin.tsx
@@ -1,19 +1,101 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
+import Checkbox from 'expo-checkbox';
 
-export const Signin: React.FC = () => {
+export const SignIn: React.FC = () => {
     return (
         <View style={styles.contentBase}>
-            <Text>Olá mundo!!</Text>
-        </View>);
+            <View style={styles.contentCenter}>
+                <Text style={styles.title}>Olá mundo</Text>
+                <Text style={styles.subTitle}>Vamos desenvolver um app?</Text>
+
+                {/* <View>
+          <Image
+            source={require("src/shared/assets/logo-mobile.ico")}
+          />
+        </View> */}
+
+                <View style={styles.form}>
+                    <TextInput
+                        placeholder="Username"
+                        style={styles.emailInput}
+                    />
+                    <TextInput
+                        placeholder="Senha"
+                        style={styles.emailPassword}
+                    />
+
+                    <View style={styles.checkboxContainer}>
+                        <Checkbox />
+                        <Text >Manter conectado</Text>
+                    </View>
+
+                    <TouchableOpacity style={styles.signInButton}>
+                        <Text style={styles.textSignInButton}>Entrar</Text>
+                    </TouchableOpacity>
+                    <Text>Não tem uma conta ainda?</Text>
+                    <TouchableOpacity style={styles.signUpButton}>
+                        <Text style={styles.textSignUpButton}>Cadastrar-se</Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+        </View>
+    );
 }
 
-const styles = StyleSheet.create(
-    {
-        contentBase: {
-            flex: 1,
-            alignItems: 'center',
-            justifyContent: 'center',
-        },
+const styles = StyleSheet.create({
+    contentBase: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#AAF',
+    },
+    contentCenter: {
+        width: '80%',
+
+    },
+    subTitle: {
+        fontWeight: '300',
+        fontSize: 22,
+    },
+    title: {
+        fontWeight: '600',
+        fontSize: 36,
+    },
+    form: {},
+    emailInput: {
+        borderColor: 'blue',
+        borderRadius: 4,
+        borderWidth: 1,
+        padding: 10,
+    },
+    emailPassword: {
+        borderColor: 'blue',
+        borderRadius: 4,
+        borderWidth: 1,
+        padding: 10,
+    },
+    checkboxContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    signInButton: {
+        padding: 10,
+        backgroundColor: 'blue',
+        borderRadius: 8,
+        alignContent: 'center',
+    },
+    signUpButton: {
+        padding: 10,
+        borderColor: 'blue',
+        borderWidth: 1,
+        borderRadius: 8,
+    },
+    textSignInButton: {
+        textAlign: 'center',
+        color: 'white'
+    },
+    textSignUpButton: {
+        textAlign: 'center',
     }
-);
+});
